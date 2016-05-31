a51d13250a360ecd3dd7db978f40c0c627830faa
Phil Fluegel <pfluegelcx@gmail.com>
2016-05-31 12:08:17 -0500

    new sites added

--- script/app.js
+++ script/app.js
@@ -1,15 +1,19 @@
 (function(){
 var $section = $('.section-left');
 var $form = $('#form')
-	if($(window).height() > 900){
+	if($(window).height() > 1100){
 			$($section).css({'left': '0'});
 		}
-	 var $scrollDistance = $section.offset().top - $(window).height() + 100;
+	 var $scrollDistance = $section.offset().top - $(window).height() + 350;
 		
 	$(window).scroll(function(event){
 		if($(window).scrollTop() >= $scrollDistance) {
 			animateLeft($section, 0);
 			animateLeft($form, 0);
+		}
+		else{
+			animateLeft($section, -600 + 'px');
+			animateLeft($form, 900 + "px");
 		}
 	})
 	
